f587361d61cdd1cbbeaf2abfdac159461d17f60a
David Enke <david@enke.dev>
2025-09-21 19:54:52 +0200

    test: align fixture

--- test/test.component.ts
+++ test/test.component.ts
@@ -5,13 +5,6 @@ import { html, LitElement, property } from 'lit';
 // eslint-disable-next-line import/no-unresolved
 import { customElement } from 'lit/decorators.js';
 
-/**
- * Graphic of a brook used as visual signature.
- *
- * @cssprop --kvlm-brook-color - Color of the brook's stroke.
- * @cssprop --kvlm-brook-opacity - Opacity of the brook.
- * @cssprop --kvlm-brook-stroke-width - Width of the brook's stroke.
- */
 @customElement('test-component')
 export class TestComponent extends LitElement {
   @property({ type: String })
