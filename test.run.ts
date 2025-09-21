90169310ceae995522d4de4c414fee4e97eecddb
David Enke <david@enke.dev>
2025-09-21 20:29:58 +0200

    test: use eslint node api

--- test.run.ts
+++ test.run.ts
@@ -1,76 +1,65 @@
 import expect from 'node:assert';
-import { exec as _exec } from 'node:child_process';
 import { describe, it } from 'node:test';
-import { promisify } from 'node:util';
 
-import type { ESLint } from 'eslint';
+import { ESLint } from 'eslint';
 
-const exec = promisify(_exec);
+async function runEslintOnFile(filePath: string, fix = false): Promise<ESLint.LintResult> {
+  const results = await new ESLint({ fix }).lintFiles([filePath]);
+  return results[0];
+}
 
-async function runLinterOnFile(filePath: string, tryFix = false): Promise<ESLint.LintResult> {
-  try {
-    const cmd = ['eslint', '--format', 'json', filePath];
-    if (tryFix) {
-      cmd.push('--fix-dry-run');
-    }
-    const { stdout } = await exec(cmd.join(' '));
-    return JSON.parse(stdout)[0];
-  } catch (error) {
-    const { stdout } = error as { stdout: string };
-    return JSON.parse(stdout)[0];
-  }
 }
 
 describe('naive check that linter grabs issues', () => {
   it('finds all issues in Typescript', async () => {
-    const { errorCount } = await runLinterOnFile('test/test.ts');
+    const { errorCount } = await runEslintOnFile('test/test.ts');
     expect.equal(errorCount, 6, 'TS file should have 6 issues');
   });
 
   it('finds all issues in markup in Typescript', async () => {
-    const { errorCount } = await runLinterOnFile('test/test.html.ts');
+    const { errorCount } = await runEslintOnFile('test/test.html.ts');
     expect.equal(errorCount, 5, 'Markup in TS file should have 5 issues');
   });
 
   it('finds all issues in Lit components', async () => {
-    const { errorCount } = await runLinterOnFile('test/test.component.ts');
+    const { errorCount } = await runEslintOnFile('test/test.component.ts');
     expect.equal(errorCount, 4, 'Lit component file should have 4 issues');
   });
 
   it('finds all issues in HTML', async () => {
-    const { errorCount } = await runLinterOnFile('test/test.html');
+    const { errorCount } = await runEslintOnFile('test/test.html');
     expect.equal(errorCount, 18, 'HTML file should have 18 issues');
   });
 
   it('finds all issues in JSON', async () => {
-    const { errorCount } = await runLinterOnFile('test/test.json');
+    const { errorCount } = await runEslintOnFile('test/test.json');
     expect.equal(errorCount, 3, 'JSON file should have 2 issues');
   });
 });
 
 describe('naive check that linter can fix issues', () => {
   it('applies fixable issues in Typescript', async () => {
-    const { errorCount } = await runLinterOnFile('test/test.ts', true);
+    const { errorCount } = await runEslintOnFile('test/test.ts', true);
     expect.equal(errorCount, 3, 'TS file should have 3 remaining unfixable issues');
   });
 
   it('applies fixable issues in markup in Typescript', async () => {
-    const { errorCount } = await runLinterOnFile('test/test.html.ts', true);
+    const { errorCount } = await runEslintOnFile('test/test.html.ts', true);
     expect.equal(errorCount, 5, 'Markup in TS file should not have been fixed');
   });
 
   it('applies fixable issues in Lit components', async () => {
-    const { errorCount } = await runLinterOnFile('test/test.component.ts', true);
+    const { errorCount } = await runEslintOnFile('test/test.component.ts', true);
     expect.equal(errorCount, 1, 'Lit component file should have 1 remaining unfixable issue');
   });
 
   it('applies fixable issues in HTML', async () => {
-    const { errorCount } = await runLinterOnFile('test/test.html', true);
+    const { errorCount } = await runEslintOnFile('test/test.html', true);
     expect.equal(errorCount, 16, 'HTML file should have 16 remaining unfixable issues');
   });
 
   it('applies fixable issues in JSON', async () => {
-    const { errorCount } = await runLinterOnFile('test/test.json', true);
+    const { errorCount } = await runEslintOnFile('test/test.json', true);
     expect.equal(errorCount, 2, 'JSON file should have 2 remaining unfixable issues');
   });
 });
